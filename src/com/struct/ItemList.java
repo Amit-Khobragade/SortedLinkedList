243bccc130d5f12fe12e993260465a6e4eec2b3d
Amit Khobragade <amitkh1102@outlook.com>
2021-01-20 09:49:56 +0530

    Added Generic objects to node and ItemList

--- src/com/struct/ItemList.java
+++ src/com/struct/ItemList.java
@@ -1,6 +1,6 @@
 package com.struct;
 
-public abstract class ItemList {
+public abstract class ItemList<T extends Comparisons<T>> {
 	///////////////////////////////////////////////////////
 	//////////////////// constructor //////////////////////
 	///////////////////////////////////////////////////////
@@ -10,18 +10,18 @@ public abstract class ItemList {
 	///////////////////////////////////////////////////////
 	////////////////////// abstract ///////////////////////
 	///////////////////////////////////////////////////////
-	abstract public void add( Item it);
-	abstract public Item remove();
-	abstract public Item get( int pos );
+	abstract public void add( T it);
+	abstract public T remove();
+	abstract public T get( int pos );
 	
 	///////////////////////////////////////////////////////
 	////////////////////// methods ////////////////////////
 	///////////////////////////////////////////////////////
-	public void setItem( Item it ) {
+	public void setItem( T it ) {
 		this.current.setItem( it );
 	}
 	
-	public Item getItem() {
+	public T getItem() {
 		return this.current.getItem();
 	}
 	
@@ -36,52 +36,52 @@ public abstract class ItemList {
 	///////////////////////////////////////////////////////
 	/////////////////////// fields  ///////////////////////
 	///////////////////////////////////////////////////////
-	protected Node start = new Node();
-	protected Node end = new Node();
-	protected Node current = new Node();
+	protected Node<T> start = new Node<T>();
+	protected Node<T> end = new Node<T>();
+	protected Node<T> current = new Node<T>();
 }
 
 
 
 
-class Node{
+class Node <T>{
 	public Node() {
 		this( null, null, null );
 	}
 	
-	public Node( Node previous, Node next, Item it ) {
+	public Node( Node<T> previous, Node<T> next, T it ) {
 		this.next = next;    
 		this.previous = previous;
 		this.it = it;
 	}
 	
-	public Item getItem() {
+	public T getItem() {
 		return this.it;
 	}
 	
-	public Node getNext() {
+	public Node<T> getNext() {
 		return this.next;
 	}
 	
-	public Node getPrevious() {
+	public Node<T> getPrevious() {
 		return this.previous;
 	}
 	
 
-	public void setItem( Item it ) {
+	public void setItem( T it ) {
 		this.it = it;
 	}
 	
-	public void setNext( Node next ) {
+	public void setNext( Node<T> next ) {
 		this.next = next;
 	}
 
-	public void setPrevious( Node previous ) {
+	public void setPrevious( Node<T> previous ) {
 		this.previous = previous;
 	}
 
-	private Item it = null;
-	private Node next = null;
-	private Node previous = null;
+	private T it = null;
+	private Node<T> next = null;
+	private Node<T> previous = null;
 	
 }
